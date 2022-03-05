835640be9518e24941939245bf8b2856fdac2acd
Maciej Ornowski <mac0201@hotmail.co.uk>
2022-03-05 15:25:01 +0000

    Top controls

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,1 +1,15 @@
 //! this file will contain helper functions and an object with colours
+const colours = {
+    RED: "#ff4b25",
+    GREEN: "#6cc230",
+    BLUE: "#0b88ba",
+    PURPLE: "#af34cd",
+    PINK: "#ff50ad",
+    YELLOW: "#d3e446",
+    ORANGE: "#fea122",
+};
+
+const clearDiv = (div_name) => {
+    div_name.innerHTML = "";
+    console.log(`${div_name} cleared.`);
+};
