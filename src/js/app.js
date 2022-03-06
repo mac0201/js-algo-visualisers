7acac5650bf4cf34839cbd124ef7946e7ad95d8d
Maciej Ornowski <mac0201@hotmail.co.uk>
2022-03-06 13:11:26 +0000

    Before changing controls

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,28 @@
 //! Main file
+const parent = document.querySelector("#chart_container");
+// Generate N random numbers between 5 and 1000. Return list of numbers
+const generateRandomList = (n) => {
+    const list = [];
+    for (let i = 0; i < n; i++) {
+        const num = Math.floor(Math.random() * (1000 - 5) + 5);
+        list.push(num);
+    }
+    return list;
+};
+
+const createBar = (value) => {
+    bar = document.createElement("div");
+    bar.className = "bar";
+    bar.style = `height: ${value / 2}px;`;
+    parent.appendChild(bar);
+};
+
+const generateChart = (numbers) => {
+    clearDiv(parent);
+    for (let i of numbers) {
+        createBar(i);
+    }
+};
+
+const GLOBAL_LIST = [];
+const TEMP_LIST = [];
--- src/js/listeners.js
+++ src/js/listeners.js
@@ -1,1 +1,14 @@
 // ! This file contains event listeners for buttons
+const btn_start = document.getElementById("btn_start");
+const slider = document.getElementById("size_slider");
+
+btn_start.addEventListener("click", () => {
+    console.log("clicked");
+});
+
+slider.addEventListener("click", () => {
+    const val = document.getElementById("slider_val").value;
+    numbers = generateRandomList(val);
+    generateChart(numbers);
+    console.log(numbers);
+});
